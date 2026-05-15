77459981ccee5f68533ec2d947c1f18d6794d7c3
Shahajadbi K Mokashi <278966384+shahzadbimokashi-rgb@users.noreply.github.com>
2026-05-15 16:20:52 +0530

    Update login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,1 +1,10 @@
-
+export default function Login() {
+  return (
+    <div>
+      <h3>Login Page (Demo)</h3>
+      <input placeholder="Email" />
+      <input placeholder="Password" />
+      <button>Login</button>
+    </div>
+  );
+}
