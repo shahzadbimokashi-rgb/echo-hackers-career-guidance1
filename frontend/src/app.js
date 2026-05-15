b79774f0cc68a0c678534ba09b3030b2a37ad156
Shahajadbi K Mokashi <278966384+shahzadbimokashi-rgb@users.noreply.github.com>
2026-05-15 16:19:15 +0530

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,15 @@
+import Chat from "./components/chat";
+import Login from "./components/login";
+import Welcome from "./components/welcome";
 
+function App() {
+  return (
+    <div>
+      <Welcome />
+      <Login />
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
