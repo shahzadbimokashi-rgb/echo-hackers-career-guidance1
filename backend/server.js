69b6a64c56d5ce46201fdbb3823a94b949dee90e
Shahajadbi K Mokashi <278966384+shahzadbimokashi-rgb@users.noreply.github.com>
2026-05-15 12:04:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Career Guidance Backend Running 🚀");
+});
+
+app.listen(process.env.PORT || 5000);
